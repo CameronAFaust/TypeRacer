ef130c9cd3ba2cb2f20fc1f64f258f8791936239
Cfaust <cfaust@student.neumont.edu>
2020-08-04 23:16:25 -0600

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ChangeDetectorRef, Input, ViewChild, ViewChildren } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Amplify, Auth } from 'aws-amplify';
 
 @Component({
   // tslint:disable-next-line: component-selector
@@ -19,9 +20,42 @@ export class LoginPopupComponent implements OnInit {
     password: new FormControl(''),
     confirmPassword: new FormControl(''),
   });
+  // , { validators: this.passwordMatchValidator });
+
+  // passwordMatchValidator(g: FormGroup) {
+  //   return g.get('password').value === g.get('passwordConfirm').value
+  //     ? null : { 'mismatch': true };
+  // }
 
   constructor(private ref: ChangeDetectorRef) { }
+
   ngOnInit() {
+    Amplify.configure({
+      Auth: {
+        identityPoolId: 'us-east-2_NgvUMaC8q', // Amazon Cognito Identity Pool ID
+        region: 'us-east-2', // Amazon Cognito Region
+      }
+    });
+  }
+
+  async login() {
+    const login = this.loginForm.value;
+    const user2 = await Auth.signIn(login.username, login.password)
+      .then(user => console.log(user))
+      .catch(err => console.log(err));
+  }
+
+  async signup() {
+    const signup = this.registerForm.value;
+    const userT = await Auth.signUp({
+      username: signup.username,
+      password: signup.password,
+      attributes: {
+        email: signup.email
+      }
+    })
+      .then(user => console.log(user))
+      .catch(err => console.log(err));
   }
 
   hide(evt) {
